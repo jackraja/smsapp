3e85ec1659dfad7ae01efaa867e9b276765ec178
jackraja <raja@caiplanet.in>
2023-07-25 14:55:44 +0530

    Update server.php

--- caisms/server.php
+++ caisms/server.php
@@ -3,10 +3,9 @@
 /**
  * Laravel - A PHP Framework For Web Artisans
  *
- * @package  Laravel
+ * @package  Laravel g
  * @author   Taylor Otwell <taylor@laravel.com>
  */
-
 $uri = urldecode(
     parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH)
 );
